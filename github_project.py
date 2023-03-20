ea8eb6f3df0eaa14626fcac19349380da6c0c4aa
linzheng1009 <sim_shang@live.com.my>
2023-03-20 15:16:07 +0800

    added university name

--- github_project.py
+++ github_project.py
@@ -1,14 +1,14 @@
 def full_information(name, age):
   full_info = f"My name is {name} and I am {age} years old"
   if age < 12:
-    school_info = "I am primary school student"
+    school_info = "I am primary school student hahaha"
   elif 12 < age < 17:
     school_info = "I am secondary school student"
   else:
     school_info = "I am a college student"
     
   return full_info + " " + school_info
-    
+# hello I am Sim  
 name = input("What is your name?") 
 age = input("How old are you?") 
 full_information(name, age) 
